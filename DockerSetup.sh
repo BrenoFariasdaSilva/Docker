67d93771599fcae6974459025a18701cd588dc1b
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-07-13 14:06:50 -0300

    FEAT: Adding the docker installation script

---
+++ DockerSetup.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Run:
+# chmod +x DockerSetup.sh
+# sudo ./DockerSetup.sh
+
+# Curl: 
+sudo apt install curl -y
+
+# Install Docker Commands:
+sudo apt update -y
+sudo apt-get install \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg \
+  lsb-release
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+sudo apt update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo apt get install docker -y
+sudo apt install docker.io -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo docker run --name hello-world-container hello-world
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+sudo docker rm hello-world-container
+sudo docker rmi hello-world
