02206f545ee07988c6521be65087ae4d41edde86
Breno Farias <brenofarias@Brenos-MacBook-Pro.local>
2024-03-13 21:35:15 -0300

    REFACTOR: Updating the usage of apt-get to apt

--- Docker-Scripts/docker-install.sh
+++ Docker-Scripts/docker-install.sh
@@ -9,7 +9,7 @@ sudo apt install curl -y
 
 # Install Docker Commands:
 sudo apt update -y
-sudo apt-get install \
+sudo apt install \
   apt-transport-https \
   ca-certificates \
   curl \
@@ -20,10 +20,10 @@ echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 sudo apt update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 sudo groupadd docker
 sudo usermod -aG docker $USER
-sudo apt get install docker -y
+sudo apt install docker -y
 sudo apt install docker.io -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
